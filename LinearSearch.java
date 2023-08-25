7997b96c3a63d19c105cbeb716754abd03538545
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-08-25 17:17:20 +0530

    lenear search

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,20 +1,11 @@
 import java.util.Scanner;
-public class ReverseArr {
+public class LinearSearch {
    
-    int[] reverseArr(int[] a) {
-        for(int i=0, j=a.length-1; i<j; i++, j--) {
-            a[i]=a[i]+a[j];
-            a[j]=a[i]-a[j];
-            a[i]=a[i]-a[j];
-        }
-        return a;
-    }
-    
+        
     int[] takeArr(int[] b) {
         Scanner sc1 = new Scanner(System.in);
         for(int i=0; i<b.length; i++)
             b[i]=sc1.nextInt();
-        
         return b;
     }        
     void printArr(int[] d) {
@@ -27,25 +18,31 @@ public class ReverseArr {
     
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        ReverseArr ra = new ReverseArr();
+        LinearSearch ra = new LinearSearch();
         
         System.out.print("Enter Array Length : ");
         int l=sc.nextInt();
-        
+                
         int[] c = new int[l];
                 
         System.out.print("Enter Array elements : ");
         c=ra.takeArr(c);
                 
-        System.out.print("Befor Reverse : ");
-        ra.printArr(c);
+        System.out.print("Present Array ");
+        ra.printArr(c);     
         
-        c=ra.reverseArr(c);
-        System.out.print("After Reverser : ");
-        ra.printArr(c);
-        System.out.println();
-        
-        
-        
+        System.out.print("Enter Searchable Element: ");
+        int s=sc.nextInt();
+        if(!ra.Searching(c, s))
+            System.out.println("Your searchable element is not found.");
+        sc.close();
+    }
+    boolean Searching(int[] arr, int j) {
+        for(int i=0; i<arr.length; i++)
+            if(j==arr[i]) {
+                System.out.println("Your searchabel Elements is index no : " + i);
+                return true;
+            }
+        return false;
     }
 }
