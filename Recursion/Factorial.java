970a7c6b29d6447ce27c2b5c1f22c403c1cd3c6b
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-09-06 17:31:41 +0530

    Factorial using recursion

---
+++ Recursion/Factorial.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the Number : ");
+        int num=sc.nextInt();
+
+        if(num<0)
+            num*=-1;
+
+        int fact=factorial(num);
+
+        System.out.println("Factorial is " + fact);
+
+    }
+
+    public static int factorial(int num) {
+
+        if(num==0)
+            return 0;
+        else if(num==1)
+            return 1;
+        else {
+            return num * factorial(num-1);
+        }
+    }
+}
