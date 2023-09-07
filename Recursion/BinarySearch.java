eade04db3576679b1763dcacdf7fda897261ecb4
RALA PREMANSHU SINGH <ralapremanshusingh@gmail.com>
2023-09-07 07:50:03 +0530

    binary search using recursion

--- Recursion/BinarySearch.java
+++ Recursion/BinarySearch.java
@@ -5,7 +5,7 @@ public class BinarySearch {
         int[] arr={1, 2, 3, 8, 9, 32521};
         int l=0, h=arr.length-1, value=9;
         int result = search(arr,l,h,value);
-        System.out.println("The value is index no : " +result);
+        System.out.println("The value at index no : " +result);
     }
 
     private static int search(int[] arr, int l, int h, int value) {
